6ff81a1d64f7ef5572a9271a64d2534fc662009b
danil <domoolink931@gmail.com>
2020-09-11 00:26:04 +0700

    First commit

--- Lab1/src/main/java/Main.java
+++ Lab1/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		try {
+			MulticastDetection multicast = new MulticastDetection("224.0.0.11", 8080);
+			multicast.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
