03eb3ee60652afd5f5591d08e0c4a6206db87eb9
danil <domoolink931@gmail.com>
2020-12-15 16:49:32 +0700

    fix

--- Lab5/src/main/java/Main.java
+++ Lab5/src/main/java/Main.java
@@ -4,15 +4,12 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            Proxy proxy = new Proxy(1488);
-            proxy.run();
-        } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			Proxy proxy = new Proxy(1488);
+			proxy.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
